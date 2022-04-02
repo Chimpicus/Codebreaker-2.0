5d3f9be221e263ca8389f8e29f0f735729e66dbf
Chimpicus <caseyboyes@gmail.com>
2022-04-02 09:39:00 +0100

    peg functionality update

--- components/ColorPeg.tsx
+++ components/ColorPeg.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-type Props = {};
+// type Props = {};
+export interface pegProps {
+    value: number;
+}
 
-function ColorPeg({}: Props) {
-  const background = 'gray-700';
-  return <div className={`h-8 w-8  border-2 border-gray-300 rounded-3xl bg-${background}`}></div>;
+function ColorPeg() {
+    let colors = ['gray-700', 'blue-400', 'green-400', 'yellow-400', 'red-500', 'indigo-400', 'teal-400', 'fuchsia-400', 'orange-500', 'white'];
+    let [peg, setPeg] = useState<pegProps>({
+        value: 0,
+    });
+    const handleClick = () => {
+        console.log('PEG CLICKED');
+        console.log('pegValue == ' + peg.value);
+        if (peg.value === 9) {
+            setPeg({
+                value: 1,
+            });
+        } else {
+            setPeg({ value: peg.value + 1 });
+        }
+        console.log(peg);
+    };
+
+    return (
+        <div
+            id='guessPeg'
+            className={`h-8 w-8 border-2 border-gray-300 rounded-3xl bg-gray-700 flex justify-center items-center`}
+            onClick={() => {
+                handleClick();
+            }}
+        >
+            <span className='flex justify-center items-center'>{peg.value}</span>
+        </div>
+    );
 }
 
 export default ColorPeg;
--- components/FeedbackPeg.tsx
+++ components/FeedbackPeg.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 type Props = {};
 
 function FeedbackPeg({}: Props) {
-  let background = 'gray-700';
-  return <div className={`h-3 w-3 border-2 border-gray-300 rounded-3xl bg-${background}`}></div>;
+    const colors = ['gray-700', 'red-600', 'amber-600', 'green-600'];
+
+    return <div id='feedbackPeg' className={`h-3 w-3 border-2 border-gray-300 rounded-3xl bg-${colors[0]}`}></div>;
 }
 
 export default FeedbackPeg;
