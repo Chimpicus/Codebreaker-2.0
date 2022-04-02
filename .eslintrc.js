d2306aab98e180814f9baa912838275a683aede8
Chimpicus <caseyboyes@gmail.com>
2022-04-02 09:39:56 +0100

    general config update

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+
+
+module.exports = {
+
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "next:next/core-web-vital",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "react/prop-types": 0,
+    "no-mixed-operators": "error",
+    "no-console": "warn",
+    "semi": [
+      "error",
+      "always"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParrens: 'always',
+    bracketSpacing: true,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    printWidth: 180,
+    tabWidth: 4,
+    trailingComma: 'es5',
+};
