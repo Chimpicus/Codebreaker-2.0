1198c8211fa46fcb0900104428f9c7b0426a4cbd
Chimpicus <caseyboyes@gmail.com>
2022-04-02 12:56:16 +0100

    update file structure

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 type Props = {};
 
 function Navbar({}: Props) {
-  return (
-    <div className='w-full px-4 h-10 flex justify-between items-center bg-gray-800 font-mono text-green-300 shadow-md shadow-gray-700'>
-      <h1 className=''>Codebreaker</h1>
-      <div className='flex px-4'>
-        <button className='px-4'>Stats</button>
-        <button className='px-4'>Settings</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className='w-full px-4 h-10 flex justify-between items-center bg-gray-800 font-mono text-green-300 shadow-md shadow-gray-700'>
+            <h1 className=''>Codebreaker</h1>
+            <div className='flex px-4'>
+                <button className='px-4'>Stats</button>
+                <button className='px-4'>Settings</button>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Gameboard from '../components/Gameboard';
+import Gameboard from '../components/codebreaker/Gameboard';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return (
-    <div className=''>
-      <Gameboard />
-    </div>
-  );
+    return (
+        <div className=''>
+            <Gameboard />
+        </div>
+    );
 };
 
 export default Home;
