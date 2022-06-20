00087eb0505aa771689e72b2dffd6625f780fefa
Casey Boyes <casey.boyes@fortiumtech.com>
2022-06-20 14:18:13 +0100

    removal of redundant gameBoardProps causing build failure

--- components/codebreaker/Gameboard.tsx
+++ components/codebreaker/Gameboard.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import GameRow from './GameRow';
 
-type GameBoardProps = {
-    id: number;
-    key: number;
-    isActive: boolean;
-};
-
-const Gameboard: React.FC<GameBoardProps> = () => {
+const Gameboard: React.FC = () => {
     return (
         <div className='flex flex-col justify-center items-center h-screen w-full'>
             {Array.from({ length: 6 }).map((gameRow, i) => (
