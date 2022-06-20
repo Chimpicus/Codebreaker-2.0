babf0d5bb50b9518c781efda32aa87645a26a76d
Casey Boyes <casey.boyes@fortiumtech.com>
2022-06-20 16:36:59 +0100

    reversed map for rows

--- components/codebreaker/Gameboard.tsx
+++ components/codebreaker/Gameboard.tsx
@@ -4,9 +4,9 @@ import GameRow from './GameRow';
 const Gameboard: React.FC = () => {
     return (
         <div className='flex flex-col justify-center items-center h-screen w-full'>
-            {Array.from({ length: 6 }).map((gameRow, i) => (
-                <GameRow key={i} id={i} isActive={false} />
-            ))}
+            {Array.from({ length: 6 })
+                .map((gameRow, i) => <GameRow key={i} id={i} isActive={false} />)
+                .reverse()}
         </div>
     );
 };
